bafb2701f9962836105ede765f289996d788753b
thatplatypus <tombrewer0@gmail.com>
2023-12-02 11:17:24 -0700

    Day 2 solution

---
+++ AdventOfCode2023/Day2/Solution.cs
@@ -0,0 +1,98 @@
+﻿using System.Drawing;
+
+namespace AdventOfCode2023.Day2
+{
+    /// <summary>
+    /// Elf color cube bag game
+    /// https://adventofcode.com/2023/day/2
+    /// </summary>
+    public class Solution
+    {
+        public static int Solve(IEnumerable<string> input)
+        {
+            int result = 0;
+            List<Dictionary<string, int>> gameTracker = new();
+            Dictionary<string, int> controlBag = new()
+            {
+                { "red",  12 },
+                { "green", 13 },
+                { "blue", 14}
+            };
+
+            ParseGame(gameTracker, input);
+
+            int index = 0;
+            gameTracker.ForEach(game =>
+            {
+                index++;
+                bool invalid = false;
+
+                foreach (var key in controlBag.Keys)
+                {
+                    if (game[key] > controlBag[key])
+                        invalid = true;
+                }
+
+                if (!invalid)
+                    result += index;
+            });
+
+            return result;
+        }
+
+        public static int Solve_Part2(IEnumerable<string> input)
+        {
+            int result = 0;
+            List<Dictionary<string, int>> gameTracker = new();
+
+            ParseGame(gameTracker, input);
+
+            gameTracker.ForEach(game =>
+            {
+                result += game["blue"] * game["red"] * game["green"];
+            });
+
+            return result;
+        }
+
+
+        public static void ParseGame(List<Dictionary<string, int>> gameTracker, IEnumerable<string> input)
+        {
+            foreach (var item in input)
+            {
+                gameTracker.Add([]);
+
+                foreach (var game in item[(item.IndexOf(":") + 1)..].Split(";"))
+                {
+                    foreach (var colorScore in game.Trim().Split(','))
+                    {
+                        string arg1 = colorScore.Trim().Split(" ")[0];
+                        string arg2 = colorScore.Trim().Split(" ")[1];
+
+                        if (int.TryParse(arg1, out int count))
+                            TryAdd(gameTracker.Last(), arg2, count);
+
+                        if (int.TryParse(arg2, out count))
+                            TryAdd(gameTracker.Last(), arg1, count);
+
+
+                    }
+                }
+            }
+        }
+
+        public static void TryAdd(Dictionary<string, int> gameTracker, string key, int count)
+        {
+            if (gameTracker.ContainsKey(key))
+            {
+                if (count > gameTracker[key])
+                    gameTracker[key] = count;
+            }
+            else
+            {
+                gameTracker.Add(key, count);
+            }
+        }
+    }
+
+}
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,8 +1,13 @@
-﻿using AdventOfCode2023.Day1;
+﻿using Day1 = AdventOfCode2023.Day1;
+using Day2 = AdventOfCode2023.Day2;
 
-var input = await File.ReadAllLinesAsync("Day1/input.txt");
+var input = await File.ReadAllLinesAsync("Day2/Input.txt");
 
-var result = Solution.Solve_Part2(input);
+//var result = Day1.Solution.Solve(input);
+//var result = Day1.Solution.Solve_Part2(input);
+
+var result = Day2.Solution.Solve(input);
+//var result = Day2.Solution.Solve_Part2(input);
 
 Console.WriteLine(result);
 
