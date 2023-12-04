287c2f19a4e439cb874107590182200f0baf4d1f
thatplatypus <tombrewer0@gmail.com>
2023-12-03 19:26:43 -0700

    Day 3 Solution

---
+++ AdventOfCode2023/Day3/Solution.cs
@@ -0,0 +1,123 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.Day3
+{
+    /// <summary>
+    /// https://adventofcode.com/2023/day/3
+    /// </summary>
+    public partial class Solution
+    {
+        
+        public static int Solve(IEnumerable<string> input)
+        {
+            int result = 0;
+            var grid = input.Select(x => x.ToCharArray()).ToArray();
+
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (char.IsDigit(grid[i][j]))
+                    {
+                        int start = j;
+                        while (j < grid[i].Length && char.IsDigit(grid[i][j]))
+                        {
+                            j++;
+                        }
+
+                        int num = int.Parse(new string(grid[i], start, j - start));
+                        if (!IsDanglingPart(grid, i, start, j - 1))
+                        {
+                            result += num;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }  
+
+        public static long Solve_Part2(IEnumerable<string> input)
+        {
+            List<Part> parts = [];
+            List<Point> gears = [];
+            int y = 0;
+
+            foreach (string line in input)
+            {
+                foreach (Match match in AllParts().Matches(line).Cast<Match>())
+                {
+                    parts.Add(new Part
+                    {
+                        PartNumber = int.Parse(match.Value),
+                        Y = y,
+                        X = match.Index,
+                        Length = match.Length
+                    });
+                }
+
+                foreach (Match match in AllGears().Matches(line).Cast<Match>())
+                {
+                    gears.Add(new Point { X = match.Index, Y = y });
+                }
+
+                y++;
+            }
+
+            long gearRatioTotal = 0;
+            foreach (var gear in gears)
+            {
+                var nearbyParts = parts.Where(part => part.IsNearby(gear.X, gear.Y)).ToList();
+                if (nearbyParts.Count == 2)
+                {
+                    gearRatioTotal += nearbyParts[0].PartNumber * nearbyParts[1].PartNumber;
+                }
+            }
+
+            return gearRatioTotal;
+        }
+
+        private static bool IsDanglingPart(char[][] grid, int i, int sj, int ej)
+        {
+            int[] dx = [-1, 0, 1, 0, -1, -1, 1, 1];
+            int[] dy = [0, 1, 0, -1, -1, 1, -1, 1];
+
+            for (int j = sj; j <= ej; j++)
+            {
+                for (int k = 0; k < 8; k++)
+                {
+                    int ni = i + dx[k], nj = j + dy[k];
+                    if (ni >= 0 && ni < grid.Length && nj >= 0 && nj < grid[0].Length)
+                    {
+                        if (grid[ni][nj] != '.' && !(ni == i && nj >= sj && nj <= ej))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        [GeneratedRegex(@"\d+")]
+        private static partial Regex AllParts();
+        [GeneratedRegex(@"\*")]
+        private static partial Regex AllGears();
+
+        partial struct Part
+        {
+            public int X;
+            public int Y;
+            public int Length;
+            public int PartNumber;
+
+            public readonly bool IsNearby(int symbolX, int symbolY)
+            {
+                if (symbolY < (Y - 1) || symbolY > (Y + 1)) return false;
+                return Enumerable.Range(X - 1, Length + 2).Contains(symbolX);
+            }
+        };
+    }
+}
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,13 +1,18 @@
 ﻿using Day1 = AdventOfCode2023.Day1;
 using Day2 = AdventOfCode2023.Day2;
+using Day3 = AdventOfCode2023.Day3;
 
-var input = await File.ReadAllLinesAsync("Day2/Input.txt");
+
+var input = await File.ReadAllLinesAsync("Day3/Input.txt");
 
 //var result = Day1.Solution.Solve(input);
 //var result = Day1.Solution.Solve_Part2(input);
 
-var result = Day2.Solution.Solve(input);
+//var result = Day2.Solution.Solve(input);
 //var result = Day2.Solution.Solve_Part2(input);
+
+//var result = Day3.Solution.Solve(input);
+var result = Day3.Solution.Solve_Part2(input);
 
 Console.WriteLine(result);
 
